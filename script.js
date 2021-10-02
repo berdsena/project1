a7561faac9289e1b14546743074c03061b1d9bbd
Your Name <fraeulein_k@gmx.net>
2021-10-02 12:14:50 +0200

    hiding menu when clicking link

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const menuOpen = document.getElementById("menuOpen");
 const menuClose = document.getElementById("menuClose");
 const menuContent = document.getElementById("menuContent");
 const loginIcon = document.getElementById("loginIcon");
+const menuLinks = document.getElementsByClassName("menu-link");
 
 menuButton.addEventListener("click", () => {
   menuOpen.classList.toggle("display-none");
@@ -12,3 +13,12 @@ menuButton.addEventListener("click", () => {
   menuContent.classList.toggle("display-none");
   loginIcon.classList.toggle("display-none");
 });
+
+for (link of menuLinks) {
+  link.addEventListener("click", () => {
+    menuOpen.classList.toggle("display-none");
+    menuClose.classList.toggle("display-none");
+    menuContent.classList.toggle("display-none");
+    loginIcon.classList.toggle("display-none");
+  });
+}
