6a22cb6ba29999444b439f85ba77a42fd3fa0971
Mez Dek <mezdekosman@hotmail.com>
2021-10-05 14:00:39 +0200

    finalised slideshow js file

--- slideshow_2.js
+++ slideshow_2.js
@@ -1,40 +1,3 @@
-// const next = document.querySelector('.next');//assigning a variable for the next button on the slide show
-// const prev = document.querySelector('.prev');//assigning a variable for the previous button on the slide show
-// const slides = document.querySelectorAll('.slide');//a matrix contains all the slides
-// const index = document.querySelectorAll('.dot');//a matrix contains all the dots below the slides
-// let slideNr=0;//default value referring to the first slide
-
-// changeSlide(slideNr,slides);
-
-// //the changeSlide function hides all slides and shows only the specific slide
-// function changeSlide (nr,array){
-//     for(let i=0;i<array.length;i++){
-//         array[i].style.display = 'none';
-//     }
-//     array[nr].style.display = 'flex';
-// }
-
-// //the goNext function adds one to the value of the current index
-// function goNext (){
-//     slideNr++;
-//     if(slideNr === index.length){slideNr = 0;};//then sets to the first value if the last value has been reached
-//     changeSlide(slideNr,slides);
-// }
-
-// //the goBack function substracts one from the value of the current index
-// function goBack (){
-//     slideNr--;
-//     if(slideNr < 0){slideNr += index.length;};//then sets to the last value if the first value has been reached
-//     changeSlide(slideNr,slides);
-// }
-
-// next.addEventListener('click', goNext);
-// prev.addEventListener('click', goBack);
-// next are event listenera to all the dots
-// for (let i = 0; i<index.length;i++) {
-//     index[i].addEventListener('click',()=>{changeSlide(i,slides)});
-// }
-
 const rss = {
     be : {
         sld : document.querySelectorAll('.beCarousel .slide'),
@@ -60,9 +23,10 @@ class Slide {
         this.currentSlide = currentSlide;
         this.prev = prev;
         this.next = next;
-        this.changeSlide();
+        this.changeSlide(this.currentSlide);
     }
-    changeSlide (){
+    changeSlide (n){
+        this.currentSlide=n;
         for(let i=0;i<this.slidesArray.length;i++){
             this.slidesArray[i].style.display = 'none';
         }
@@ -71,12 +35,20 @@ class Slide {
     goBack (){
         this.currentSlide--;
         if(this.currentSlide < 0){this.currentSlide += this.slidesIndex.length;};//then sets to the last value if the first value has been reached
-        this.changeSlide();
+        this.changeSlide(this.currentSlide);
     }
     goNext (){
         this.currentSlide++;
         if(this.currentSlide === this.slidesIndex.length){this.currentSlide = 0;};//then sets to the first value if the last value has been reached
-        this.changeSlide();
+        this.changeSlide(this.currentSlide);
+    }
+    nav (i) {
+        if (i == 0){
+            this.goBack()
+        } else{this.goNext()}
+    }
+    ind (n) {
+        this.changeSlide(n);
     }
 }
 
@@ -90,23 +62,24 @@ for (let area in rss){
             if(type === 'nav'){
                 switch (area){
                     case 'be':
-                        rss[area][type][i].addEventListener('click',beCarousel.changeSlide);
+                        rss[area][type][i].addEventListener('click',()=>{beCarousel.nav(i)});
                         break;
                     case 'do':
-                        rss[area][type][i].addEventListener('click',doCarousel.changeSlide);
+                        rss[area][type][i].addEventListener('click',()=>{doCarousel.nav(i)});
                         break;
                     default:
-                        rss[area][type][i].addEventListener('click',goCarousel.changeSlide);
-                }
+                        rss[area][type][i].addEventListener('click',()=>{goCarousel.nav(i)});
+                };
+            } else if (type === 'ind') {
                 switch (area){
                     case 'be':
-                        rss[area][type][i].addEventListener('click',()=>{beCarousel.currentSlide = i; changeSlide});
+                        rss[area][type][i].addEventListener('click',()=>{beCarousel.ind(i)});
                         break;
                     case 'do':
-                        rss[area][type][i].addEventListener('click',()=>{doCarousel.currentSlide = i; changeSlide});
+                        rss[area][type][i].addEventListener('click',()=>{doCarousel.ind(i)});
                         break;
                     default:
-                        rss[area][type][i].addEventListener('click',()=>{goCarousel.currentSlide = i; changeSlide});
+                        rss[area][type][i].addEventListener('click',()=>{goCarousel.ind(i)});
                 };                
             };
         };
